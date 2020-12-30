197dfa3212bb59478e98f5024ff81f3c594b42df
Dan Maina <dannymaina@gmail.com>
2020-12-30 22:23:28 +0300

    adds sign up and sign in POST routes

--- adonisgram/start/routes.ts
+++ adonisgram/start/routes.ts
@@ -2,5 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.on('/').render('welcome')
 
-Route.get('signup', 'AuthController.signup').as('signup')
-Route.get('signin', 'AuthController.signin').as('signin')
+Route.get('signup', 'AuthController.show_signup_form').as('show_signup_form')
+Route.post('signup', 'AuthController.signup_user').as('signup_user')
+Route.get('signin', 'AuthController.show_signin_form').as('show_signin_form')
+Route.post('signin', 'AuthController.signin_user').as('signin_user')
