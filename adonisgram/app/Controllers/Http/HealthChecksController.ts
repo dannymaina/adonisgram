f42186ec39cb3aee94b734e63721a5e755f743e7
Dan Maina <dannymaina@gmail.com>
2020-12-31 00:52:50 +0300

    refactors health check to health check controller

---
+++ adonisgram/app/Controllers/Http/HealthChecksController.ts
@@ -0,0 +1,11 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
+
+
+export default class HealthChecksController {
+    public async index ({response}: HttpContextContract) {
+        const report = await HealthCheck.getReport()
+        return report.healthy ? response.ok(report) : response.badRequest(report)
+      }
+}
+
--- adonisgram/start/routes.ts
+++ adonisgram/start/routes.ts
@@ -1,13 +1,4 @@
 import Route from '@ioc:Adonis/Core/Route'
-import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
-
-Route.get('health', async ({ response }) => {
-  const report = await HealthCheck.getReport()
-  
-  return report.healthy
-    ? response.ok(report)
-    : response.badRequest(report)
-})
 
 Route.on('/').render('welcome')
 
@@ -15,3 +6,5 @@ Route.get('signup', 'AuthController.show_signup_form').as('show_signup_form')
 Route.post('signup', 'AuthController.signup_user').as('signup_user')
 Route.get('signin', 'AuthController.show_signin_form').as('show_signin_form')
 Route.post('signin', 'AuthController.signin_user').as('signin_user')
+
+Route.get('health', 'HealthChecksController.index').as('health_check')
